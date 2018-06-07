faba05a97cae7b1cf5d935d964128a4b31c13e78
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2018-06-07 17:30:10 +0530

    function scopes

--- 01UILayer/01JavaScript/test.js
+++ 01UILayer/01JavaScript/test.js
@@ -1,4 +1,4 @@
-console.log("hello world!!");
+// console.log("hello world!!");
 
 var sum = function(a,b)
 {
@@ -6,8 +6,28 @@ var sum = function(a,b)
 }
 
 function outerFn(){
-	console.log('outer function')
+    console.log('outer function')
 	return function(){
     	console.log('inner function')
     }
 }
+
+//Scopes
+
+// 1. Creation Phase:
+// outerFn [x: undefined, inner:undefined] , innerFn [x:undefined]
+// 2. Execution Phase
+// outerFn [x: 2, inner:fn] , innerFn [x:undefined]
+
+function outerFn(){
+    var x = 2
+    console.log('outer function')
+	var inner = function(){
+        console.log(x+2)
+        var x = 0
+    	console.log('inner function')
+    }
+    return inner;
+}
+
+// outerFn()()
